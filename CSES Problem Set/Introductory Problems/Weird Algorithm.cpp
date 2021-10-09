411b6c70dfe6a42adb33fcafdb76c9a14f4e23ea
kwsong0113 <53707540+kwsong0113@users.noreply.github.com>
2021-10-09 15:54:41 +0900

    Update Weird Algorithm.cpp

--- CSES Problem Set/Introductory Problems/Weird Algorithm.cpp
+++ CSES Problem Set/Introductory Problems/Weird Algorithm.cpp
@@ -1,1 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long n;
 
+int main(){
+  
+  cin >> n;
+  cout << n << " ";
+  while (n > 1){
+    if (n & 1) n = 3 * n + 1;
+    else n /= 2;
+    cout << n << " ";
+  }
+}
