bfc42a833c3a89d8da79d37f5edcbb1fc787bd3b
kwsong0113 <53707540+kwsong0113@users.noreply.github.com>
2021-10-24 23:44:27 +0900

    Upload Static Range Sum Queries.cpp

---
+++ CSES Problem Set/Static Range Sum Queries.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 200001;
+int n, q, a, b, x;
+long long sum[N];
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> q;
+    for (int i = 1; i <= n; ++i){
+        cin >> x;
+        sum[i] = sum[i - 1] + x;
+    }
+    while (q--){
+        cin >> a >> b;
+        cout << (sum[b] - sum[a - 1]) << "\n";
+    }    
+}
