9c2c83401384a9420cc531ebe90ac4d5681c427d
Kiwhan Song <kwsong0113@gmail.com>
2021-12-21 19:48:33 +0900

    Update Crazy Robot.cpp

--- Codeforces/Educational Round #118/Crazy Robot.cpp
+++ Codeforces/Educational Round #118/Crazy Robot.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 pair<int, int> delta[4] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 int t, n, m, lx, ly;
+string grid[1000001];
 
-bool freeCell(char **grid, int x, int y){
-    return (x >= 1 && x <= n && y >= 1 && y <= m && grid[x][y] == '.');
+bool freeCell(string grid[], int x, int y){
+    return (x >= 0 && x < n && y >= 0 && y < m && grid[x][y] == '.');
 }
 
-int numFreeNeigbors(char **grid, int x, int y){
+int numFreeNeigbors(string grid[], int x, int y){
     int num = 0;
     for (auto [dx, dy]: delta){
         if (freeCell(grid, x + dx, y + dy)) num ++;
@@ -15,7 +16,7 @@ int numFreeNeigbors(char **grid, int x, int y){
     return num;
 }
 
-void dfs(char **grid, int x, int y){
+void dfs(string grid[], int x, int y){
     for (auto [dx, dy]: delta){
         if (freeCell(grid, x + dx, y + dy) && numFreeNeigbors(grid, x + dx, y + dy) <= 1){
             grid[x + dx][y + dy] = '+';
@@ -30,20 +31,17 @@ int main(){
     cin >> t;
     while (t--){
         cin >> n >> m;
-        char **grid;
-        grid = new char *[n + 1];
-        for (int i = 0; i <= n; ++i) grid[i] = new char[m + 1];
-        for (int i = 1; i <= n; ++i) cin >> (grid[i] + 1);
-        for (int i = 1; i <= n; ++i){
-            for (int j = 1; j <= m; ++j){
+        for (int i = 0; i < n; ++i) cin >> grid[i];
+        for (int i = 0; i < n; ++i){
+            for (int j = 0; j < m; ++j){
                 if (grid[i][j] == 'L'){
                     lx = i; ly = j; break;
                 }
             }
         }
         dfs(grid, lx, ly);
-        for (int i = 1; i <= n; ++i){
-            cout << (grid[i] + 1) << "\n";
+        for (int i = 0; i < n; ++i){
+            cout << grid[i] << "\n";
         }
     }
 }
